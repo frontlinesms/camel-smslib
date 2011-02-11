a9ce7c33914ad70e142226ffc9b2067d78f75ea9
Alex Anderson <alex@frontlinesms.com>
2011-02-11 14:38:36 +0000

    Added unit tests for CServiceFactory.

--- src/main/java/net/frontlinesms/camel/smslib/CServiceFactory.java
+++ src/main/java/net/frontlinesms/camel/smslib/CServiceFactory.java
@@ -8,8 +8,27 @@ import java.util.Map;
 import org.smslib.CService;
 
 class CServiceFactory {
+	private static final int DEFAULT_BAUD = 57600;
+
 	public CService create(String uri, String remaining, Map<String, Object> parameters) {
-		// TODO Auto-generated method stub
-		return null;
+		return new CService(remaining,
+				getBaud(parameters),
+				getString(parameters, "manufacturer"),
+				getString(parameters, "model"),
+				getString(parameters, "handler"));
+	}
+	
+	private String getString(Map<String, Object> parameters, String key) {
+		String m  = (String) parameters.get(key);
+		return m == null ? "" : m;
+	}
+
+	private int getBaud(Map<String, Object> parameters) {
+		String baud = (String) parameters.get("baud");
+		try {
+			return Integer.parseInt(baud);
+		} catch(Exception e) {
+			return DEFAULT_BAUD;
+		}
 	}
 }
---
+++ src/test/java/net/frontlinesms/camel/smslib/CServiceFactoryTest.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package net.frontlinesms.camel.smslib;
+
+import static org.powermock.api.mockito.PowerMockito.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.smslib.CService;
+
+/**
+ * Unit tests for {@link CServiceFactory}.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(CServiceFactory.class)
+public class CServiceFactoryTest {
+	private static final Map<String, Object> NO_PARAMS = Collections.emptyMap();
+	
+	private CServiceFactory factory;
+	
+	@Before
+	public void setUp() {
+		factory = new CServiceFactory();
+	}
+
+	@Test
+	public void testDefaultParamterValues() throws Exception {
+		// when
+		whenNew(CService.class).withArguments(anyString(), anyInt(),
+				anyString(), anyString(), anyString()).thenReturn(null);
+		factory.create("smslib://COM1", "COM1", NO_PARAMS);
+		
+		// then
+		verifyNew(CService.class).withArguments("COM1", 57600, "", "", "");
+	}
+	
+	@Test
+	public void testParameterPassing() throws Exception {
+		// when
+		whenNew(CService.class).withArguments(anyString(), anyInt(),
+				anyString(), anyString(), anyString()).thenReturn(null);
+		factory.create("smslib://COM1", "COM1", new ParameterMap(
+				"baud", "9600",
+				"manufacturer", "Nokia",
+				"model", "1200",
+				"handler", "Nokia_S40_3ed"));
+		
+		// then
+		verifyNew(CService.class).withArguments("COM1", 9600, "Nokia", "1200", "Nokia_S40_3ed");
+	}
+}
+
+@SuppressWarnings("serial")
+class ParameterMap extends HashMap<String, Object> {
+	ParameterMap(String... keysAndValues) {
+		assert((keysAndValues.length & 1) == 0);
+		for (int i = 0; i < keysAndValues.length; i+=2) {
+			put(keysAndValues[i], keysAndValues[i+1]);
+		}
+	}
+}
